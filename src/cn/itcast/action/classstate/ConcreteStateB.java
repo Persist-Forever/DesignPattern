da34bfce884e6c06b309fb802120964bd2f73218
Persist-Forever <wzy_yong@163.com>
2019-03-27 12:12:08 +0800

    修改后提交一次

--- src/cn/itcast/action/classstate/ConcreteStateB.java
+++ src/cn/itcast/action/classstate/ConcreteStateB.java
@@ -4,7 +4,7 @@ public class ConcreteStateB implements State{
 
 	@Override
 	public void handle(String sampleParameter) {
-		System.out.println("ConcreteStateB 状态对应的处理行为。。。。");
+		System.out.println("ConcreteStateB 状态对应的处理行为。。。。" + sampleParameter);
 		
 	}
 	
